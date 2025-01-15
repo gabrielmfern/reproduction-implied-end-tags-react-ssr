4dfc041e843c00c8c49a00222dc6acf08a470cb7
gabriel miranda <gabrielmfern@outlook.com>
2025-01-15 13:38:00 -0300

    use express for simplicity

--- index.tsx
+++ index.tsx
@@ -1,41 +1,41 @@
-import { Hono } from "hono"
-import { stream } from "hono/streaming"
-import { renderToReadableStream } from "react-dom/server"
+import express from "express"
+import { renderToPipeableStream } from "react-dom/server"
 
-const app = new Hono();
+const app = express();
 
-app.get('/', (res) => {
-  return stream(res, async (s) => {
-    const readable = await renderToReadableStream(
+app.get('/', (req, res) => {
+  const pipeable = renderToPipeableStream(
+    <p>
+      First
+      <div>...</div>
+      Text in between paragraphs
+    </p>
+  );
+
+  /*
+    If you were to directly curl into http://localhost:3000 you would get
+
+    <p>
+      First
+      <div>...</div>
+      Text in between paragraphs
+    </p>
+
+    Once it gets to the browser, it gets modified to
+
+    <...>
       <p>
         First
-        <div>...</div>
-        Text in between paragraphs
       </p>
-    );
-
-    /*
-      If you were to directly curl into http://localhost:3000 you would get
-
-      <p>
-        First
-        <div>...</div>
-        Text in between paragraphs
-      </p>
-
-      Once it gets to the browser, it gets modified to
-
-      <...>
-        <p>
-          First
-        </p>
-        <div>...</div>
-        Text in between paragraphs
-        <p></p>
-      </...>
-    */
-    await s.pipe(readable);
-  });
+      <div>...</div>
+      Text in between paragraphs
+      <p></p>
+    </...>
+  */
+  pipeable.pipe(res);
 });
 
-export default app;
+app.listen(3000, () => {
+  console.info('listening on http://localhost:3000');
+});
+
